351303ededa0c0ce9d83d2c0de632a26a1c0c4cb
Michał Hejduk <michal3run@gmail.com>
2018-10-07 17:03:09 +0200

    QueueHelper, AddRange method

---
+++ TaskSchedulerExperiment/TaskSchedulerData/Helpers/QueueHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TaskSchedulerData.Helpers
+{
+    public static class QueueHelper
+    {
+        public static void AddRange<T>(this Queue<T> queue, IEnumerable<T> enu)
+        {
+            foreach (var obj in enu)
+                queue.Enqueue(obj);
+        }
+    }
+}
