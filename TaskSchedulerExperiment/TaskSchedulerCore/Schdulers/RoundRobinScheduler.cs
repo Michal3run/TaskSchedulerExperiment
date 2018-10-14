133cfd1ec52d322c0a57bd4d3ea77acbd8954a3b
Michał Hejduk <michal3run@gmail.com>
2018-10-14 09:21:22 +0200

    RoundRobin some fixes, processingTime as float, setting WatingTime to determine if task is delayed

--- TaskSchedulerExperiment/TaskSchedulerCore/Schdulers/RoundRobinScheduler.cs
+++ TaskSchedulerExperiment/TaskSchedulerCore/Schdulers/RoundRobinScheduler.cs
@@ -13,12 +13,12 @@ namespace TaskSchedulerCore.Schdulers
         {
             if (ReadyTasks.Count == 0) return;
             
-            var processingTime = 1 / ReadyTasks.Count;
+            var processingTime = 1 / (float)ReadyTasks.Count;
 
             while (ReadyTasks.Any())
             {
                 TrySetCurrentTaskFromReadyTasks();
-                ProcessCurrentTask(processingTime);
+                ProcessCurrentTask(currentTime, processingTime);
             }
 
             //ReadyTasks queue is empty, we add tasks that are not finished in current iteration, for next iterations
@@ -26,18 +26,26 @@ namespace TaskSchedulerCore.Schdulers
             _tasksNoFinishedInCurrentIteration.Clear();
         }
 
-        private void ProcessCurrentTask(float processingTime)
+        private void ProcessCurrentTask(int currentTime, float processingTime)
         {
             CurrentTask.ProcessedTime += processingTime;
 
             if (CurrentTask.IsDone)
             {
+                CurrentTask.WaitingTime = GetTaskWaitingTime(currentTime, CurrentTask);
                 AddCurrentTaskToDone();
             }
             else
             {
                 _tasksNoFinishedInCurrentIteration.Add(CurrentTask);
             }
+        }
+
+        private float GetTaskWaitingTime(int currentTime, SchedulerTask task)
+        {
+            var timeFromCreate = currentTime + 1 - task.CreateTime;
+            var waitingTime = timeFromCreate - task.ProcessedTime; //time when task is processed is not counted as waitingTime
+            return waitingTime;
         }
     }
 }
