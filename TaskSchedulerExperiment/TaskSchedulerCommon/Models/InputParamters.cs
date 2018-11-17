5eeefe70b12f4bc1f3a6c27e65ddceea2a7522ff
Michał Hejduk <michal3run@gmail.com>
2018-11-17 14:01:01 +0100

    InputParameters added

---
+++ TaskSchedulerExperiment/TaskSchedulerCommon/Models/InputParamters.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskSchedulerCommon.Models
+{
+    public class InputParamters
+    {
+        public string OriginalFileName { get; set; }
+        public decimal Load { get; set; }
+        public int TaskLength { get; set; }
+        public int Delay { get; set; }
+        public ESchedulerType SchedulerType { get; set; }
+    }
+}
