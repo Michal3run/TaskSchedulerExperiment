f0b1f79ce67349add561cec84777997a9959023d
Michał Hejduk <michal3run@gmail.com>
2018-11-17 14:01:32 +0100

    Processing output info added

--- TaskSchedulerExperiment/TaskSchedulerCommon/Models/ProcessingOutput.cs
+++ TaskSchedulerExperiment/TaskSchedulerCommon/Models/ProcessingOutput.cs
@@ -1,9 +1,20 @@
-﻿using System.Collections.Generic;
-
-namespace TaskSchedulerCommon.Models
+﻿namespace TaskSchedulerCommon.Models
 {
     public class ProcessingOutput
     {
+        public InputParamters InputParamters { get; set; }
+        
         public decimal PercentOfDelayedTasks { get; set; }
+
+        public string GetOutputTextInfo()
+        {
+            return string.Join(" ; ",
+                InputParamters.Load,
+                InputParamters.TaskLength,
+                InputParamters.Delay,
+                InputParamters.SchedulerType.ToString(),
+                PercentOfDelayedTasks,
+                InputParamters.OriginalFileName);
+        }
     }
 }
